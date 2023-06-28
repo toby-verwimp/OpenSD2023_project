1c324021e922594b1418052028f7122969f1c7b3
Gasper Krivic <52493759+gasperkrivic@users.noreply.github.com>
2023-06-28 14:29:18 +0200

    Added title and graph labels to the plot.

--- opensd_project/visualize.py
+++ opensd_project/visualize.py
@@ -23,6 +23,9 @@ def show_reference_image(images, points=[], roi_size=None, bit_depth=12):
 
     if points:
         ax.scatter(np.array(points)[:, 1], np.array(points)[:, 0], marker='.', color='r')
+        ax.set_title('Reference image with selected points')
+        ax.set_xlabel('x^2')
+        ax.set_ylabel('y**2')
         
         if roi_size is not None:
             for point in np.array(points):
